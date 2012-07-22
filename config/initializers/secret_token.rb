b1ac60f98c79549a20104cebfdb262dfc28194cb
Ajmal Afif <ajmalafif@gmail.com>
2012-07-22 23:59:55 +0800

    fresh install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kraffy::Application.config.secret_token = '82ca50ec691c21fe7fd505f73aa4d5f251e0d8cf45ebf73c8fcfe6c2daa18862b4f92e01d5c348d1c0cb36d756f3ba65c0cc0c458b6b32845b19b562ca7831d4'
